0d718eadae2665471f451a8c1e2783b52aebf89e
woody35545 <woody35545@gmail.com>
2023-02-11 02:20:41 +0900

    [+] Add JPA setting and Entity for test

---
+++ src/main/java/User.java
@@ -0,0 +1,25 @@
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table
+@Getter
+@Setter
+public class User {
+
+    @Id
+    @Column(name = "user_id", nullable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 50)
+    private String email;
+
+    @Column(nullable = false, length = 20)
+    private String password;
+
+    @Column(nullable = false, length = 20)
+    private String name;
+
+}
