2c6b9efc72647fbeae79bba59882176f49e31bd1
woody35545 <woody35545@gmail.com>
2023-02-11 17:35:55 +0900

    [+] Temporarily implementing QuestionService, just for test

--- src/main/java/QuestionService.java
+++ src/main/java/QuestionService.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class QuestionService {
+import com.woodysite.board.Question;
+import com.woodysite.board.QuestionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class QuestionService {
+    @Autowired
+    QuestionRepository questionRepository;
+
+    public String getQuestionContent(){
+        List<Question> all = questionRepository.findAll();
+        Question res = all.get(0);
+        return res.getQuestionContent();
+    }
+
 }
