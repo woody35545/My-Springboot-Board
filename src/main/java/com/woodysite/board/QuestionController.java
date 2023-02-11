64b36b7c57a021beff5f95cb779daf7da4402adb
woody35545 <woody35545@gmail.com>
2023-02-11 16:33:16 +0900

    [+] Mapping question list

--- src/main/java/com/woodysite/board/QuestionController.java
+++ src/main/java/com/woodysite/board/QuestionController.java
@@ -1,4 +1,15 @@
 package com.woodysite.board;
 
-public class QuestionController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class QuestionController
+{
+    @ResponseBody
+    @GetMapping("/question/list")
+    public String getQuestionList(){
+        return "Question will return";
+    }
 }
