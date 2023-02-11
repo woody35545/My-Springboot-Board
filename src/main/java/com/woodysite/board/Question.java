7f4a54c4be57160e0b4f6b2a910752e9a8e60e55
woody35545 <woody35545@gmail.com>
2023-02-11 16:44:53 +0900

    Define entity's fields and mapping with existing columns in DB Server's table

--- src/main/java/com/woodysite/board/Question.java
+++ src/main/java/com/woodysite/board/Question.java
@@ -1,5 +1,23 @@
 package com.woodysite.board;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "question_table")
 public class Question {
+    @Id
+    private int questionID;
+
+    @Column(name="question_id")
+    private String questionContent;
+
+    @Column(name="category")
+    private String questionCategory;
+
+    @Column(name="selected_count")
+    private int questionSelectedCount;
 
 }
